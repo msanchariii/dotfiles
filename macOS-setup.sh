40d034a8c103befcf2725b95a24bbd6928907450
Suyash Bhawsar <suyashbhawsar@hotmail.com>
2024-06-07 20:48:54 +0530

    Update macOS-setup.sh

--- macOS-setup.sh
+++ macOS-setup.sh
@@ -1,18 +1,35 @@
+#!/bin/bash
+
 # Check if Homebrew is installed
 if ! command -v brew >/dev/null 2>&1; then
   echo -e "\033[1;34mInstalling Homebrew\033[0m"
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
   echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> ~/.zprofile
   eval "$(/opt/homebrew/bin/brew shellenv)"
+else
+  echo -e "\033[1;34mHomebrew is already installed\033[0m"
 fi
 
-# Install Python3
-echo -e "\033[1;34mInstalling Python\033[0m"
-brew install python
+# Install Python3 if not installed
+if ! command -v python3 >/dev/null 2>&1; then
+  echo -e "\033[1;34mInstalling Python\033[0m"
+  brew install python
+else
+  echo -e "\033[1;34mPython is already installed\033[0m"
+fi
 
-# Install Ansible
-echo -e "\033[1;34mInstalling Ansible\033[0m"
-brew install ansible
+# Install Ansible if not installed
+if ! command -v ansible >/dev/null 2>&1; then
+  echo -e "\033[1;34mInstalling Ansible\033[0m"
+  brew install ansible
+else
+  echo -e "\033[1;34mAnsible is already installed\033[0m"
+fi
 
-# Install Stow
-brew install stow
+# Install Stow if not installed
+if ! command -v stow >/dev/null 2>&1; then
+  echo -e "\033[1;34mInstalling Stow\033[0m"
+  brew install stow
+else
+  echo -e "\033[1;34mStow is already installed\033[0m"
+fi
